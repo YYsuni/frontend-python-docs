45140208f1d56d318370653fcdd96a25f866fd2a
Suni <YYsuni1001@gmail.com>
2026-05-10 00:51:38 +0800

    First commit

---
+++ lib/shared.ts
@@ -0,0 +1,10 @@
+export const appName = '前端 Python 3.12'
+export const docsRoute = '/docs'
+export const docsImageRoute = '/og/docs'
+export const docsContentRoute = '/llms.mdx/docs'
+
+export const gitConfig = {
+	user: 'yysuni',
+	repo: 'frontend-python-docs',
+	branch: 'main'
+}
